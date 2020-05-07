102494433044b5bfdedfcef1271c9e9f625b1b86
Brandon Hicks <bhicks1305@gmail.com>
2020-05-07 18:11:25 -0400

    changed to Get to be able to filter by status or a search

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 const { v1: uuidv1 } = require('uuid');
 
 @Injectable()
@@ -9,6 +10,23 @@ export class TasksService {
 
     getAllTasks():Task[] {
         return this.tasks;
+    }
+
+    getTaksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if(status){
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if(search){
+            tasks = tasks.filter(task => task.title.includes(search)||
+            task.description.includes(search),);
+        }
+
+        return tasks;
     }
 
     getTaskById(id: string): Task{

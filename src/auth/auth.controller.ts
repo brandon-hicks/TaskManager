cefe47965af425524bfde4b543348ca725f23299
Brandon Hicks <bhicks1305@gmail.com>
2020-07-05 23:56:30 -0400

    added the signUp feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authservice: AuthService,
+    ) {}
+
+    @Post('/auth')
+    signUp(@Body() authcredentialsDto: AuthCredentialsDto) {
+        return this.authservice.signUp(authcredentialsDto);
+    }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,16 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./auth-credentials.dto";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+     async signUp(authCradentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCradentialsDto;
 
+        const user = new User();
+        user.userName = username;
+        user.password = password;
+
+        await user.save();
+     }
 }
